fa7aeedc6c131ad3b5e4adbe2ad3da7606fb3455
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-08-10 22:21:34 +0200

    Cleaning of routes.

--- config/routes.js
+++ config/routes.js
@@ -17,24 +17,36 @@ var adminAuth = [auth.requiresLogin, auth.requiresAdmin];
 
 module.exports = function (app, passport) {
 
-  // user routes
+  // Logged out
+  app.get('/', home.index);
+  app.get('/soon', home.soon);
   app.get('/login', users.login);
+
+  // Logged in
+  app.get('/dashboard', loggedInAuth, dashboard.index);
+  app.get('/welcome', loggedInAuth, home.welcome);
+
+  app.post('/requests', loggedInAuth, requests.create);
+  app.get('/request/:id', loggedInAuth, requests.show);
+  app.post('/requests/:id/comments', loggedInAuth, comments.create);
+
+  app.get('/settings', loggedInAuth, settings.index);
+  app.post('/settings', loggedInAuth, settings.update);
+  app.get('/settings/preferences', loggedInAuth, settings.preferences);
+  app.get('/settings/subscription', loggedInAuth, settings.subscription);
+  app.get('/settings/billing', loggedInAuth, settings.billing);
+
   app.get('/signup', users.signup);
   app.get('/logout', users.logout);
   app.post('/users', users.create);
+  app.get('/user/activate/:token', auth.requiresLogin, users.activate);
   app.post('/users/session',
     passport.authenticate('local', {
       failureRedirect: '/login',
       failureFlash: 'Invalid email or password.'
     }), users.session);
 
-  app.get('/settings', auth.requiresLogin, settings.index);
-  app.post('/settings', auth.requiresLogin, settings.update);
-  app.get('/settings/preferences', auth.requiresLogin, settings.preferences);
-  app.get('/settings/subscription', auth.requiresLogin, settings.subscription);
-  app.get('/settings/billing', auth.requiresLogin, settings.billing);
-
-
+  // Auth
   app.get('/auth/facebook',
     passport.authenticate('facebook', {
       scope: [ 'email', 'user_about_me'],
@@ -60,20 +72,6 @@ module.exports = function (app, passport) {
       failureRedirect: '/login'
     }), users.authCallback);
 
-  app.param('userId', users.user);
-
-  app.post('/requests', auth.requiresLogin, requests.create);
-  app.get('/request/:id', requests.show);
-
-  app.param('id', requests.load);
-
-  app.get('/', home.index);
-  app.get('/soon', home.soon);
-
-  app.get('/dashboard', dashboard.index);
-
-  // comment routes
-  app.post('/requests/:id/comments', auth.requiresLogin, comments.create);
   // Admin
   app.get('/admin', adminAuth, admin.index);
   app.get('/admin/user/:user_id/activate', adminAuth, admin.activateUser);
