d17dc783f259bc5af6b175def8532b1f3f317339
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-09-04 10:04:35 +0200

    Bring back full height image instead of video.

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,12 @@
 $(function() {
 
+  $(window).resize(function() {
+    height = windowHeight();
+    $(".full-bg-banner").css("height", height+1);
+  });
+
+  $(window).trigger('resize');
+
   $('.read-more-button').on('click', function(){
       $('html, body').animate({
           scrollTop: $( $.attr(this, 'href') ).offset().top
@@ -7,14 +14,23 @@ $(function() {
       return false;
   });
 
+  // Set a timeout...
+  setTimeout(function(){
+    // Hide the address bar!
+    window.scrollTo(0, 1);
+  }, 0);
+
   runAnimation();
 });
 
+var windowHeight = function() {
+  var zoomLevel = document.documentElement.clientHeight / window.innerHeight;
+  return window.innerHeight * zoomLevel;
+};
 
 $('.invite-email-form').submit(function(e) {
 
   e.preventDefault();
-
 
   var email = $('.invite-email-form .input-email').val();
   var csrf = $('.invite-email-form input[name=_csrf]').val();
